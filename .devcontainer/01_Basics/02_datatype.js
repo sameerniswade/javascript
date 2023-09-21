72a6c55364263d4df30af1a260580701abd4e007
sameer <38357131+sameerniswade@users.noreply.github.com>
2023-09-21 06:59:15 +0000

    learn datatype

---
+++ .devcontainer/01_Basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all the JS code as new version
+
+//alert(3+3)  using node js not broweser
+
+console.log(3 + 3)
+
+let name = "sameer"
+let age = 25
+let isLogIn = true
+
+// Datatype
+// number => 2 to power 53
+// bigint
+// string
+// boolean 
+// null  - standalone value person should be assign 
+// undefined - machine assign  
+
+//object
+
+console.log(typeof null) // return object
+console.log(typeof undefined) // return undefined
+
+
