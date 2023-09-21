754955563d2bd710c09c5c31b124e86b61b6fe05
sameer <38357131+sameerniswade@users.noreply.github.com>
2023-09-21 06:20:31 +0000

    learn javascript variables

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1234 // only available in scope and cannot be change 
+let accountEmail = "sameer@gmail.com" // only available in scope and can be change 
+var accountPassword = "sameer@123" // 
+accountCity = "wardha" // bad way  declear variable and it is globaly available and can chane anywhere
+let accountState; // return undefine
+// accountId = 1233 not allowed
+
+/* Prefer not user var 
+   Because of issume in block scope and funtional scope
+*/
+console.table([accountEmail,accountId, accountPassword, accountCity, accountState])
