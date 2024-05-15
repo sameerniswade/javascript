e53d55c5b7ac753d28c612d5894b26bb5984e8bc
sameer <niswadesameer@gmail.com>
2024-05-15 14:16:41 +0530

    string complete

---
+++ .devcontainer/01_Basics/06_String.js
@@ -0,0 +1,20 @@
+
+const name = "Sameer"
+
+console.log(name.charAt(2))
+console.log(name.indexOf('m'))
+
+
+const subString = name.substring(2, 4)
+
+console.log(subString);
+
+
+const stringSlice = name.slice(-4, 4)
+
+console.log(stringSlice);
+
+// slice
+// splice
+//trim
+//replace
