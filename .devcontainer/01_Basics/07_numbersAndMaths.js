fe6afcf679f63c021982e8db16f1636d9b133e95
sameer <niswadesameer@gmail.com>
2024-05-15 18:30:15 +0530

    Javascript Math

---
+++ .devcontainer/01_Basics/07_numbersAndMaths.js
@@ -0,0 +1,35 @@
+const number = 400;
+
+const Number1 = new Number(400)
+
+console.log(number.toFixed(2)) // 400.00
+
+console.log(123.598.toPrecision(2)); //1.2e+2
+
+const largernumber = 100000000
+console.log(largernumber.toLocaleString("en-IN"))
+
+
+// max and min
+
+// number.max 
+
+
+//---------------------------------------Maths----------------------------
+
+
+// Maths.abs(-4) // 4
+
+// Maths.round(4.6) // 5
+
+// Maths.ceil(4.2) // 5
+// Maths.floor(4.7) // 4
+
+
+// Maths.random(1,  6)
+
+
+console.log("----------------------------------------------------------------------")
+
+
+console.log(Math.floor((Math.random() * 20) + 1));
